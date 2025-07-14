17d0a7510b4c260d982299b344b1992dd42d8f02
yangwei <yangweic@adtec.com.cn>
2025-07-14 16:30:23 +0800

    feat: router mod to createWebHashHistory

--- src/route/router.ts
+++ src/route/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Periodict from '@/components/periodict.vue'
 import AspectRatio from '@/components/aspectRatio.vue'
 import Example from '@/components/exmaple.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
