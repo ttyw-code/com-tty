f87b896813e661a9bf2e880902d53cb41d229b06
yangwei <yangweic@adtec.com.cn>
2025-12-12 14:30:02 +0800

    fix: .

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import store from './store'
 //引入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-expect-error
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
@@ -20,11 +19,9 @@ applySavedTheme()
 
 import gloablComponent from '@/components/index'
 
-// import { WebSocketManager } from '@/utils/webSocket/webSocketManager.js'
 
 const app = createApp(App)
   .use(router)
-  // .use(WebSocketManager.getInstance())
   .use(store)
 app.use(ElementPlus, {
   locale: zhCn,
